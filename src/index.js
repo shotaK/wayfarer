d55b32001ba4f901ecd9d17054beea5c5899c0f7
Shota <sh.karkashadze@gmail.com>
2022-06-20 10:11:05 +0400

    Hint markers and their actions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 (function () {
     console.log("clock");
     // document.addEventListener("load", function () {
+    var hintsActivated = false;
     var HINT_MARKER_CONTAINER_CLASSNAME = "wayfarer-hint-marker-container";
     var HINT_MARKER_CLASSNAME = "wayfarer-hint-marker";
+    // const WAYFARER_TYPING_INPUT = "wayfarer-typing-input";
+    // const createTypingInput = (): HTMLInputElement => {
+    //   const typingInput = document.createElement("input");
+    //   typingInput.type = "hidden";
+    //   typingInput.setAttribute("name", "wayfarer-typing-input");
+    //   typingInput.classList.add(WAYFARER_TYPING_INPUT);
+    //   document.body.appendChild(typingInput);
+    //   return typingInput;
+    // };
+    // const focusToTypingInput = (typingInput: HTMLInputElement): void => {
+    //   if (typingInput) {
+    //     typingInput.focus();
+    //   }
+    // };
+    // const listenToTypingInput = (typingInput: HTMLInputElement): void => {
+    //   console.log(typingInput);
+    //   if (typingInput) {
+    //     typingInput.addEventListener("input", (e) => {
+    //       console.log("input", e);
+    //       e.preventDefault();
+    //       e.stopPropagation();
+    //     });
+    //   }
+    // };
     var createHintMarkerContainer = function () {
         var hintMarkerContainer = document.createElement("div");
         hintMarkerContainer.classList.add(HINT_MARKER_CONTAINER_CLASSNAME);
         return hintMarkerContainer;
     };
-    var loadHintMarkerIntoBody = function () {
+    var loadHintMarkerContainer = function () {
         var hintMarkerContainer = createHintMarkerContainer();
         document.body.appendChild(hintMarkerContainer);
+        return hintMarkerContainer;
     };
     var isVisible = function (element) {
         return (element.offsetWidth > 0 ||
@@ -27,7 +53,7 @@
         return hintMarker;
     };
     var getAllHyperlinks = function () {
-        return Array.from(document.querySelectorAll("a"));
+        return Array.from(document.querySelectorAll("a")).filter(isVisible);
     };
     var getCoords = function (elem) {
         var box = elem.getBoundingClientRect();
@@ -41,12 +67,9 @@
         var left = box.left + scrollLeft - clientLeft;
         return { top: Math.round(top), left: Math.round(left) };
     };
-    var renderHintMarkers = function () {
-        getAllHyperlinks()
-            .filter(isVisible)
-            .forEach(function (hyperlink, index) {
+    var renderHintMarkers = function (hintMarkerContainer) {
+        getAllHyperlinks().forEach(function (hyperlink, index) {
             var coords = getCoords(hyperlink);
-            var hintMarkerContainer = document.querySelector(".".concat(HINT_MARKER_CONTAINER_CLASSNAME));
             if (hintMarkerContainer) {
                 var hintMarker = createHintMarker({
                     markKey: index.toString(),
@@ -58,11 +81,28 @@
         });
     };
     document.addEventListener("keydown", function (event) {
+        var _a;
+        if (hintsActivated) {
+            if (event.key === "Escape") {
+                hintsActivated = false;
+            }
+            else {
+                var hyperlink = getAllHyperlinks()[Number(event.key)];
+                var href = hyperlink === null || hyperlink === void 0 ? void 0 : hyperlink.href;
+                if (href) {
+                    (_a = window.open(href, "_blank")) === null || _a === void 0 ? void 0 : _a.focus();
+                }
+            }
+        }
         if (event.key === "f") {
             var allHyperlinks = getAllHyperlinks();
             if (allHyperlinks.length > 0) {
-                loadHintMarkerIntoBody();
-                renderHintMarkers();
+                var hintMarkerContainer = loadHintMarkerContainer();
+                renderHintMarkers(hintMarkerContainer);
+                hintsActivated = true;
+                // const typingInput = createTypingInput();
+                // focusToTypingInput(typingInput);
+                // listenToTypingInput(typingInput);
             }
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,37 @@
   console.log("clock");
   // document.addEventListener("load", function () {
 
+  let hintsActivated = false;
+
   const HINT_MARKER_CONTAINER_CLASSNAME = "wayfarer-hint-marker-container";
   const HINT_MARKER_CLASSNAME = "wayfarer-hint-marker";
+  // const WAYFARER_TYPING_INPUT = "wayfarer-typing-input";
+
+  // const createTypingInput = (): HTMLInputElement => {
+  //   const typingInput = document.createElement("input");
+  //   typingInput.type = "hidden";
+  //   typingInput.setAttribute("name", "wayfarer-typing-input");
+  //   typingInput.classList.add(WAYFARER_TYPING_INPUT);
+  //   document.body.appendChild(typingInput);
+  //   return typingInput;
+  // };
+
+  // const focusToTypingInput = (typingInput: HTMLInputElement): void => {
+  //   if (typingInput) {
+  //     typingInput.focus();
+  //   }
+  // };
+
+  // const listenToTypingInput = (typingInput: HTMLInputElement): void => {
+  //   console.log(typingInput);
+  //   if (typingInput) {
+  //     typingInput.addEventListener("input", (e) => {
+  //       console.log("input", e);
+  //       e.preventDefault();
+  //       e.stopPropagation();
+  //     });
+  //   }
+  // };
 
   const createHintMarkerContainer = () => {
     const hintMarkerContainer = document.createElement("div");
@@ -11,9 +40,10 @@
     return hintMarkerContainer;
   };
 
-  const loadHintMarkerIntoBody = () => {
+  const loadHintMarkerContainer = () => {
     const hintMarkerContainer = createHintMarkerContainer();
     document.body.appendChild(hintMarkerContainer);
+    return hintMarkerContainer;
   };
 
   const isVisible = (element: HTMLElement) => {
@@ -42,7 +72,7 @@
   };
 
   const getAllHyperlinks = () => {
-    return Array.from(document.querySelectorAll("a"));
+    return Array.from(document.querySelectorAll("a")).filter(isVisible);
   };
 
   const getCoords = (elem: HTMLElement) => {
@@ -63,33 +93,44 @@
     return { top: Math.round(top), left: Math.round(left) };
   };
 
-  const renderHintMarkers = () => {
-    getAllHyperlinks()
-      .filter(isVisible)
-      .forEach((hyperlink, index) => {
-        const coords = getCoords(hyperlink);
-        const hintMarkerContainer = document.querySelector(
-          `.${HINT_MARKER_CONTAINER_CLASSNAME}`
-        );
-        if (hintMarkerContainer) {
-          const hintMarker = createHintMarker({
-            markKey: index.toString(),
-            topPos: coords.top,
-            leftPos: coords.left,
-          });
+  const renderHintMarkers = (hintMarkerContainer: HTMLDivElement) => {
+    getAllHyperlinks().forEach((hyperlink, index) => {
+      const coords = getCoords(hyperlink);
+      if (hintMarkerContainer) {
+        const hintMarker = createHintMarker({
+          markKey: index.toString(),
+          topPos: coords.top,
+          leftPos: coords.left,
+        });
 
-          hintMarkerContainer.appendChild(hintMarker);
-        }
-      });
+        hintMarkerContainer.appendChild(hintMarker);
+      }
+    });
   };
 
   document.addEventListener("keydown", (event) => {
+    if (hintsActivated) {
+      if (event.key === "Escape") {
+        hintsActivated = false;
+      } else {
+        const hyperlink = getAllHyperlinks()[Number(event.key)];
+        const href = hyperlink?.href;
+        if (href) {
+          window.open(href, "_blank")?.focus();
+        }
+      }
+    }
+
     if (event.key === "f") {
       const allHyperlinks = getAllHyperlinks();
 
       if (allHyperlinks.length > 0) {
-        loadHintMarkerIntoBody();
-        renderHintMarkers();
+        const hintMarkerContainer = loadHintMarkerContainer();
+        renderHintMarkers(hintMarkerContainer);
+        hintsActivated = true;
+        // const typingInput = createTypingInput();
+        // focusToTypingInput(typingInput);
+        // listenToTypingInput(typingInput);
       }
     }
   });
